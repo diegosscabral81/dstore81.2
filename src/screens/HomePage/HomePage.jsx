d80696467842f078a0c619112f1c1ba4f1eecc40
diegosscabral81 <127567372+diegosscabral81@users.noreply.github.com>
2024-07-05 14:11:12 -0300

    Add files via upload

---
+++ src/screens/HomePage/HomePage.jsx
@@ -0,0 +1,107 @@
+import React from "react";
+import "./style.css";
+
+export const HomePage = () => {
+  return (
+    <div className="home-page">
+      <div className="overlap">
+        <img className="vector" alt="Vector" src="/img/vector.png" />
+        <img className="bolsa" alt="Bolsa" src="/img/bolsa-2.png" />
+        <img className="heart" alt="Heart" src="/img/icons8-heart-32-1-2.png" />
+        <div className="overlap-group">
+          <img className="search" alt="Search" src="/img/icons8-search-50-2.png" />
+          <div className="text-wrapper">O que você procura?</div>
+        </div>
+        <div className="div">Atendimento</div>
+        <div className="text-wrapper-2">Compre pelo WhatsApp</div>
+        <div className="text-wrapper-3">DSTORE81</div>
+        <img className="headset" alt="Headset" src="/img/headset.png" />
+        <img className="whats-app" alt="Whats app" src="/img/whatsapp.png" />
+      </div>
+      <div className="overlap-2">
+        <div className="rectangle" />
+        <img className="IMAGEM" alt="Imagem" src="/img/imagem-2-2.png" />
+        <p className="p">Em um raio de 50 Km*</p>
+      </div>
+      <div className="overlap-3">
+        <img className="in-transit" alt="In transit" src="/img/icons8-in-transit-32-2.png" />
+        <div className="text-wrapper-4">FRETE GRÁTIS*</div>
+        <p className="text-wrapper-5">Compras acima de R$ 250,00*</p>
+      </div>
+      <div className="overlap-4">
+        <div className="text-wrapper-6">Digite o seu CEP</div>
+        <div className="text-wrapper-6">Digite o seu CEP</div>
+      </div>
+      <div className="OUTLET">LANÇAMENTOS</div>
+      <div className="masculino">FEMININO</div>
+      <div className="infantil">INFANTIL</div>
+      <div className="overlap-5">
+        <img className="img" alt="Rectangle" src="/img/rectangle-24.svg" />
+        <img className="rectangle-2" alt="Rectangle" src="/img/rectangle-22.png" />
+        <img className="rectangle-3" alt="Rectangle" src="/img/rectangle-31.png" />
+        <img className="rectangle-4" alt="Rectangle" src="/img/rectangle-28.png" />
+        <img className="rectangle-5" alt="Rectangle" src="/img/rectangle-29.png" />
+        <img className="rectangle-6" alt="Rectangle" src="/img/rectangle-23.png" />
+        <div className="text-wrapper-7">Novidades</div>
+        <div className="text-wrapper-8">FLAMENGO 22/23</div>
+        <div className="text-wrapper-9">199,99</div>
+        <div className="text-wrapper-10">PALMEIRAS 22/23</div>
+        <div className="text-wrapper-11">199,99</div>
+        <img className="hearts" alt="Hearts" src="/img/hearts-4.png" />
+        <img className="hearts-2" alt="Hearts" src="/img/hearts-4.png" />
+        <img className="hearts-3" alt="Hearts" src="/img/hearts-4.png" />
+        <img className="hearts-4" alt="Hearts" src="/img/hearts-4.png" />
+        <img className="hearts-5" alt="Hearts" src="/img/hearts-4.png" />
+        <div className="text-wrapper-12">CORINTHIANS 21/22</div>
+        <div className="text-wrapper-13">199,99</div>
+        <div className="text-wrapper-14">PSG 22/23</div>
+        <div className="text-wrapper-15">199,99</div>
+        <div className="text-wrapper-16">REAL MADRID 22/23</div>
+        <div className="text-wrapper-17">R$ 199,99</div>
+        <img className="rectangle-7" alt="Rectangle" src="/img/rectangle-58.png" />
+        <img className="rectangle-8" alt="Rectangle" src="/img/rectangle-58.png" />
+        <img className="rectangle-9" alt="Rectangle" src="/img/rectangle-58.png" />
+        <img className="rectangle-10" alt="Rectangle" src="/img/rectangle-58.png" />
+        <img className="rectangle-11" alt="Rectangle" src="/img/rectangle-58.png" />
+      </div>
+      <div className="place-marker-wrapper">
+        <img className="place-marker" alt="Place marker" src="/img/place-marker-1.png" />
+      </div>
+      <div className="overlap-6">
+        <div className="rectangle-12" />
+        <img className="bank-cards" alt="Bank cards" src="/img/bank-cards.png" />
+        <p className="text-wrapper-18">PARCELE EM ATÉ 12X SEM JUROS</p>
+      </div>
+      <div className="overlap-group-2">
+        <img className="i-phone" alt="I phone" src="/img/iphone.png" />
+        <p className="text-wrapper-19">Baixe o nosso App e tenha descontos exclusivos</p>
+      </div>
+      <div className="overlap-7">
+        <div className="rectangle-13" />
+        <img className="rectangle-14" alt="Rectangle" src="/img/rectangle-33.svg" />
+        <div className="text-wrapper-20">FORMAS DE PAGAMENTO</div>
+        <img className="rectangle-15" alt="Rectangle" src="/img/rectangle-54.png" />
+        <div className="text-wrapper-21">FALE CONOSCO</div>
+        <div className="text-wrapper-22">LOCALIZAÇÃO</div>
+        <div className="AV-UM-CUIAB-MT">
+          AV UM-001
+          <br />
+          CUIABÁ-MT
+          <br />
+          78000-000
+        </div>
+        <div className="element">
+          65 999999999
+          <br />
+          dstore81@gmail.com
+        </div>
+        <p className="text-wrapper-23">
+          Os preços, promoções, condições de pagamento, frete e produtos são válidos exclusivamente para compras
+          realizadas via internet.Fotos meramente ilustrativas. Copyright © - dstore.com.br. Todos os direitos
+          reservados.
+        </p>
+      </div>
+      <div className="feminino">MASCULINO</div>
+    </div>
+  );
+};
